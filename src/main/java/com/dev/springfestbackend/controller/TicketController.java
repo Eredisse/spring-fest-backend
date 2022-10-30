1ae49176eed188bc40ca7a974cccdc91aaff22ff
alexandru.borza <alexandru.borza@tazz.ro>
2022-10-30 18:16:18 +0200

    add validation

--- src/main/java/com/dev/springfestbackend/controller/TicketController.java
+++ src/main/java/com/dev/springfestbackend/controller/TicketController.java
@@ -4,10 +4,13 @@ import com.dev.springfestbackend.dto.GenerateTicketsDTO;
 import com.dev.springfestbackend.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tickets")
@@ -17,9 +20,21 @@ public class TicketController {
     private final TicketService ticketService;
 
     @PostMapping("/generate")
-    public HttpStatus generateTickets(@RequestBody GenerateTicketsDTO generateTicketsDTO) {
+    public HttpStatus generateTickets(@RequestBody @Valid GenerateTicketsDTO generateTicketsDTO) {
         ticketService.generateTickets(generateTicketsDTO);
         return HttpStatus.OK;
     }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+        List<String> error = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(constraintViolation -> {
+            error.add(constraintViolation.getPropertyPath() + ": " + constraintViolation.getMessage());
+        });
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 
 }
--- src/main/java/com/dev/springfestbackend/dto/GenerateTicketsDTO.java
+++ src/main/java/com/dev/springfestbackend/dto/GenerateTicketsDTO.java
@@ -3,13 +3,18 @@ package com.dev.springfestbackend.dto;
 import com.dev.springfestbackend.enums.TicketTypeValues;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 public class GenerateTicketsDTO {
 
     private TicketTypeValues ticketType;
 
+    @Positive @NotNull
     private Long numberOfTickets;
 
+    @Positive @NotNull
     private Long pricePerTicket;
 
 }
